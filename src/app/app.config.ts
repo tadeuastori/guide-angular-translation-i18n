efc135e0a8a58b45da55868cca48fb5d23ed1403
Carlos Astori <tadeu.astori@gmail.com>
2026-06-17 12:30:08 -0400

    installed transloco

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['pt-br', 'en-us'],
+        defaultLang: 'pt-br',
+        // Remove this option if your application doesn't support changing language in runtime.
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
+  ],
 };
---
+++ transloco.config.ts
@@ -0,0 +1,9 @@
+import { TranslocoGlobalConfig } from '@jsverse/transloco-utils';
+
+const config: TranslocoGlobalConfig = {
+  rootTranslationsPath: 'public/i18n/',
+  langs: ['pt-br', 'en-us'],
+  keysManager: {},
+};
+
+export default config;
